f28df9bb6ada20f8b22339d194d97afb5a55813a
Nick <nicknelson1021@gmail.com>
2019-02-03 15:09:37 -0600

    updates

--- app/src/main/java/org/nicknelson/fragmentexample/SimpleFragment.java
+++ app/src/main/java/org/nicknelson/fragmentexample/SimpleFragment.java
@@ -6,19 +6,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class SimpleFragment extends Fragment {
+
+    private static final int YES = 0;
+    private static final int NO = 1;
 
     public SimpleFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View rootView =
+                inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                View radioButton = radioGroup.findViewById(checkedId);
+                int index = radioGroup.indexOfChild(radioButton);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
+
+                switch (index) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
+        return rootView;
     }
 
 }
